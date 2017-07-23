6657268ad063b6ce0a494170c36630556fe3f0d9
Gaurav Bansal <gaurav2.bansal@intel.com>
2017-07-23 14:25:47 -0700

    Added comments to the model.py file

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# import required modules
 import csv
 import os
 import cv2
@@ -11,17 +12,20 @@ print(keras.__version__)
 import sklearn
 samples = []
 
+# read the csv file containing image locations/names and corresponding steering angles
 with open('p3-data-23/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
+# split the samples into training and validation sets
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 print('Number of training samples = ', len(train_samples))
 print('Number of validation samples = ', len(validation_samples))
 
+# define the generator function
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
@@ -31,6 +35,8 @@ def generator(samples, batch_size=32):
 
             images = [] 
             measurements = []
+            # note that we are using all three, i.e. left, center, and right images
+            # the steering angle for left and right images are corrected for steering angle
             for batch_sample in batch_samples:
                 steering_center = float(batch_sample[3])
                 correction = 0.3
@@ -52,6 +58,7 @@ def generator(samples, batch_size=32):
                 measurement = steering_right
                 measurements.append(measurement)
 
+            # The images are augmented by flipping each image and adding to the training set
             augmented_images, augmented_measurements = [], []
             for image,measurement in zip(images,measurements):
                 augmented_images.append(image)
@@ -69,11 +76,18 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
+#import required modules and layers from keras
 from keras.models import Sequential, Model
 from keras.layers import Cropping2D
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Conv2D, MaxPooling2D, Activation
 
+#the NN architecture consists of:
+#a. Cropping layer to crop the top part of the image
+#b. Scaling and centering of the image pixel values
+#c. 4 convlution2D layers, each followed by a maxpool layer
+#d. A flatten layer
+#e. 5 dense or fully connected layers
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
@@ -96,6 +110,7 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+#We use the mean-square-error loss function and Adam optimizer
 model.compile(loss='mse', optimizer='adam')
 #model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 history_object = model.fit_generator(train_generator, steps_per_epoch=len(train_samples)/32
@@ -106,6 +121,7 @@ model.save('model.h5')
 
 print(history_object.history.keys())
 
+#The code below plots the history of convergence of the model
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
 plt.title('model mean squared error loss')
